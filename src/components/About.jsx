ad8a3c363151b488ee56ece7f64385e13da59a88
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2023-03-06 00:12:50 +0530

    modified styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -35,7 +35,7 @@ const ServiceCard = ({ index, title, icon }) => {
 const About = () => {
   return (
     <>
-      <motion.div variants={textVariant()} className="mt-10">
+      <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
